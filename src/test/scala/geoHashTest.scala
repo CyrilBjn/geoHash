01447fe26d58fe8ab0dfd5a2ca9396ed6711ec23
CyrilBjn <cyril.brajon78@gmail.com>
2022-05-03 14:20:40 +0200

    get ready for mentoring

--- src/test/scala/geoHashTest.scala
+++ src/test/scala/geoHashTest.scala
@@ -1,35 +1,11 @@
 package octo.mentoring.project
 
-import hash.GeoHash.{Interval, encode, mixList, toBit}
-import hash.Base32
 import org.scalatest.funsuite.AnyFunSuite
 
 class geoHashTest extends AnyFunSuite{
 
   test("Sequence de booléen en base 32"){
-    val boolSeq = Seq(true, false, true, true, false)
-    val c = Base32.toBase32(boolSeq)
-    assert(c == 'q')
-  }
-
-  test("Encoder le 34 OCTO"){
-    val latitude = 48.868670
-    val longitude = 2.333380
-    val hashCode = encode(latitude, longitude)
-    assert(hashCode == "u09wj070esq5xh")
-  }
-
-  test("Encoder en Bit un double"){
-    val latitude = 48.868670
-    val bits = toBit(latitude, Interval(-90.0, 90.0), 12)
-    assert(bits == List(true, true, false, false, false, true, false, true, true, false, false, false))
-  }
-
-  test("Mixer 2 listes en alternance"){
-    val list1 = List("a","c","e")
-    val list2 = List("b","d","f")
-    val mixedList = mixList(list1, list2)
-    assert(mixedList == List("a", "b", "c", "d", "e", "f"))
+    assert('q' == 'q')
   }
 
 }
